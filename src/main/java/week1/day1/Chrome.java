388f7123e305bc7a64f487bf31e5f6d39d5cbcaa
yravikum <yaswaantravikumar@gmail.com>
2025-02-01 19:53:22 +0530

    Initial Commit
    First Commit 01-Feb-2025

---
+++ src/main/java/week1/day1/Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0F;
+		System.out.println("Browser Version is : " + version + "\n");
+
+		String developer = "Google";
+		System.out.println("Browser Name is : " + developer + "\n");
+
+		boolean isBeta = false;
+		System.out.println("Is browser a beta version : " + isBeta + "\n");
+
+		short releaseYear = 2008;
+		System.out.println("Release Version of Browser is : " + releaseYear + "\n");
+
+		char shortcutKey = 'C';
+		System.out.println("ShorcutKey is : " + shortcutKey);
+	}
+
+}
